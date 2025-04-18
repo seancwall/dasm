57248abfa3e8f2486549ba02400dfcb8235d702f
Sean Wall <sean.wall@baesystems.us>
2025-04-18 15:05:42 -0400

    Add helper code

---
+++ machines/nes/helpers.asm
@@ -0,0 +1,54 @@
+; Helper subroutines
+
+ClearRAM subroutine
+        lda #0
+        tax
+.clearRAM
+        sta $00,X
+        cpx #$FE
+        bcs .skipStack
+        sta $100,x
+.skipStack
+        sta $200,x
+        sta $300,x
+        sta $400,x
+        sta $500,x
+        sta $600,x
+        sta $700,x
+        inx
+        bne .clearRAM
+        rts
+
+WaitSync subroutine
+        bit PPU_STATUS
+        bpl WaitSync
+        rts
+
+NextRandom subroutine
+        lsr
+        bcc .NoEor
+        eor #$D4
+.NoEor  rts
+
+PrevRandom subroutine
+        asl
+        bcc .NoEor
+        eor #$A9
+.NoEor  rts
+
+ReadJoypad0 subroutine
+        ldy #0
+ReadJoypadY
+        lda #$01
+        sta JOYPAD1,y
+        lsr
+        sta JOYPAD1,y
+        ldx #8
+.loop   pha
+        lda JOYPAD1,y
+        lsr
+        pla
+        rol
+        dex
+        bne .loop
+        rts
